dd3b3c97a28d711f646bb13aab5e054ad85364f9
marcphilippebeaujean <marcphilippebeaujean@gmail.com>
2026-09-07 20:54:12 +0200

    chore: order Todoist module exports

--- src/todoist/module.ts
+++ src/todoist/module.ts
@@ -1,7 +1,7 @@
-export { registerTodoistMergeConsumer } from "./merge-consumer.ts";
 export { maybeAnalyzeTaskClaim } from "./claiming.ts";
 export type { TaskClaimResultEvent } from "./claiming-flow.ts";
 export {
 	handleTaskClaimResult,
 	runTaskClaim,
 } from "./claiming-flow.ts";
+export { registerTodoistMergeConsumer } from "./merge-consumer.ts";
